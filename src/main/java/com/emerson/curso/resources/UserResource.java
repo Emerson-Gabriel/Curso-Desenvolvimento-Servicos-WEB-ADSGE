4567bee0a24a5c0508af2faa25b9aaba9830d875
Emerson-Gabriel <emersong730@gmail.com>
2019-12-16 10:13:59 -0300

    Pre-authorizing requests
    Desafio vídeo 06-09.

--- src/main/java/com/emerson/curso/resources/UserResource.java
+++ src/main/java/com/emerson/curso/resources/UserResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,10 +30,13 @@ public class UserResource {
 	private UserService service;
    
 	@GetMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<List<UserDTO>> findAll(){
 		List<UserDTO> list= service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	
 	@GetMapping(value ="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
 		
@@ -48,6 +52,8 @@ public class UserResource {
 		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDto);
 	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value="/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
