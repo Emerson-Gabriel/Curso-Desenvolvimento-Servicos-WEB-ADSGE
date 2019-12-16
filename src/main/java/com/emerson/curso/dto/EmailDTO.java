6b256439e2a71df171307f750e232ead633c258f
Emerson-Gabriel <emersong730@gmail.com>
2019-12-16 11:26:11 -0300

    Forgot password, Logger
    Vídeo aula 06-13.

---
+++ src/main/java/com/emerson/curso/dto/EmailDTO.java
@@ -0,0 +1,30 @@
+package com.emerson.curso.dto;
+
+import java.io.Serializable;
+
+public class EmailDTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String email;
+
+	public EmailDTO() {
+
+	}
+
+	public EmailDTO(String email) {
+
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
